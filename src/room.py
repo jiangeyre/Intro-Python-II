d60cd86444e5090c8b303cdba9cc78afe1146c5e
Lexie Jiang <lexiemingjiang@gmail.com>
2020-04-08 14:34:17 -0700

    Commit 5: added items to Room

--- src/room.py
+++ src/room.py
@@ -3,9 +3,10 @@
 from player import Player
 
 class Room:
-    def __init__(self, name, description):
+    def __init__(self, name, description, items=[]):
         self.name = name
         self.description = description
+        self.items = items
         self.n_to = None
         self.s_to = None
         self.e_to = None
@@ -13,3 +14,10 @@ class Room:
 
     def __str__(self):
         return f"{self.name} \n {self.description}"
+
+    def room_inventory(self):
+        if self.items is None:
+            print('There are no items in this room.')
+        else:
+            for item in self.items:
+                print(f"There is a {item.name} in this room!")
